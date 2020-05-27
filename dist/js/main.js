c4e8477ad4dccdd31014e2873c28d86c88e538ea
syczu92 <syczu92@gmail.com>
2020-05-27 20:12:25 +0200

    Your commit message

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,3 +9,44 @@ $(window).resize(function() {
       });
   }
 });
+
+$('.labels-slider').slick({
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+$('.social-media__slider').slick({
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
